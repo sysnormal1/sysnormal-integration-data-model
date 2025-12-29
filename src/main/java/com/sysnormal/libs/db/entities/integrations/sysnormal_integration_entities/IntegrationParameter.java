fe1786dd100fc203078783e6a7d6a5d91c262b28
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2025-12-29 11:23:14 -0300

    added integration parameter

---
+++ src/main/java/com/sysnormal/libs/db/entities/integrations/sysnormal_integration_entities/IntegrationParameter.java
@@ -0,0 +1,41 @@
+package com.sysnormal.libs.db.entities.integrations.sysnormal_integration_entities;
+
+import com.sysnormal.libs.db.entities.base_entities.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "integration_parameters")
+@Getter
+@Setter
+public class IntegrationParameter extends BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, scale = 6)
+    private LocalDateTime createdAt;
+
+    @Column(name = "parameter_type", nullable = false, length = 100)
+    @ColumnDefault("TEXT")
+    private String parameterType = "TEXT";
+
+    @Column(name = "parameter_name", nullable = false, length = 4000, unique = true)
+    private String parameterName;
+
+    @Column(name = "parameter_value", length = 4000)
+    private String parameterValue;
+
+    protected static final long TABLE_ID = 35001;
+    public static long getTableId() {
+        return TABLE_ID;
+    }
+}
